495183c7c3c263cfb2d541b2b6c51d0189945451
Rick Roche <richard.j.roche@gmail.com>
2018-01-01 02:08:10 +0200

    chore: update dock prefs

--- macos/dock.sh
+++ macos/dock.sh
@@ -7,10 +7,10 @@ dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/Applications/Microsoft Outlook.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/Postman.app"
-dockutil --no-restart --add "/Applications/SoftwareAG/Designer/Eclipse.app"
+dockutil --no-restart --add "/Applications/SoftwareAG912/Designer/Eclipse.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications/Notability.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
-dockutil --no-restart --add "/Applications/1Password 7.app"
+dockutil --no-restart --add '~/Downloads' --view fan --display stack --allhomes
 
 killall Dock
