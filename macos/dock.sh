88eb65ae24856ff761a861650930d22c452f97da
Rick Roche <richard.j.roche@gmail.com>
2018-08-07 08:24:49 +0200

    feat: update the dock to reflect my preferences

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/Launchpad.app"
+dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/Applications/Mail.app"
-dockutil --no-restart --add "/Applications/Calendar.app"
-dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
+dockutil --no-restart --add "/Applications/Microsoft Outlook.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+dockutil --no-restart --add "/Applications/Postman.app"
+dockutil --no-restart --add "/Applications/SoftwareAG/Designer/Eclipse.app"
+dockutil --no-restart --add "/Applications/iTerm.app"
+dockutil --no-restart --add "/Applications/Notability.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
-dockutil --no-restart --add "/Applications/Spotify.app"
+dockutil --no-restart --add "/Applications/1Password 7.app"
 
 killall Dock
