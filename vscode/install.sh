e2a74ff2c763e08ae233455b96730eb6ac83e595
Rick Roche <richard.j.roche@gmail.com>
2019-07-07 12:58:02 +0200

    feat: backup vscode using settings sync

--- vscode/install.sh
+++ vscode/install.sh
@@ -2,25 +2,5 @@
 
 cp settings.json "$HOME/Library/Application Support/Code/User/settings.json"
 
-code --install-extension Arjun.swagger-viewer
-code --install-extension bbenoist.vagrant
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension EditorConfig.EditorConfig
-code --install-extension eg2.tslint
-code --install-extension jebbs.plantuml
-code --install-extension mjmcloug.vscode-elixir
-code --install-extension mrmlnc.vscode-scss
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension nobuhito.printcode
-code --install-extension PeterJausovec.vscode-docker
-code --install-extension rbbit.typescript-hero
-code --install-extension redhat.java
-code --install-extension sammkj.vscode-elixir-formatter
-code --install-extension sasa.vscode-sass-format
-code --install-extension vscjava.vscode-java-debug
-code --install-extension vscjava.vscode-java-pack
-code --install-extension vscjava.vscode-java-test
-code --install-extension vscjava.vscode-maven
-code --install-extension wmaurer.change-case
-code --install-extension yzane.markdown-pdf
-code --install-extension yzhang.markdown-all-in-one
+# My settings are now saved to Github using this plugin
+code --install-extension Shan.code-settings-sync
