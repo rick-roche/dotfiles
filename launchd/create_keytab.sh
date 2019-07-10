01f144e74c8eb338b2c5d9cc21bcec9b89829bbf
Rick Roche <richard.j.roche@gmail.com>
2019-07-10 09:12:54 +0200

    refactor: determine the kerberos principle programatically

--- launchd/create_keytab.sh
+++ launchd/create_keytab.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 DIR=$(dirname "$0")
-PRINCIPLE="$1"
+ENC_TYPE=aes256-cts-hmac-sha1-96
+
+if [ -z "$1" ]; then
+    NAME=$(echo "$USER" | cut -d "." -f 1 | awk '{for(i=1;i<=NF;i++){ $i=toupper(substr($i,1,1)) substr($i,2) }}1')
+    SURNAME=$(echo "$USER" | cut -d "." -f 2 | awk '{for(i=1;i<=NF;i++){ $i=toupper(substr($i,1,1)) substr($i,2) }}1')
+    DOMAIN=$(awk '/domain/ {print $2}' /etc/resolv.conf)
+    PRINCIPLE="$NAME.$SURNAME@$DOMAIN"
+else
+    PRINCIPLE=$1
+fi
 
 ktutil -k "$DIR/$USER.keytab" \
-    add -p "$PRINCIPLE" -V 1 -e aes256-cts-hmac-sha1-96
+    add -p "$PRINCIPLE" -V 1 -e $ENC_TYPE
