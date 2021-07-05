36dc009383ea07df615ac5cdae60980670b89e0f
Richard Roché <richard.j.roche@gmail.com>
2021-07-05 08:32:45 +0200

    chore: remove vscode setup after move to official settings sync

--- vscode/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cp settings.json "$HOME/Library/Application Support/Code/User/settings.json"
-
-# My settings are now saved to Github using this plugin
-code --install-extension Shan.code-settings-sync
