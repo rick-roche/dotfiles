3993d883af37a4c5312ba4086f1e197db665797a
Richard Roché <richard.j.roche@gmail.com>
2022-04-15 11:24:28 +0100

    chore: add work setup

---
+++ settings/_work-setup.zsh
@@ -0,0 +1,14 @@
+export DOTFILES_PERSONALITY=work
+export EMAIL_ADDRESS=richard.j.roche@gmail.com
+export GPG_KEY=E52FCBDA63052DDBC77567EC684AF18D79E744E6
+
+export DOTFILES_MODULES=(
+    homebrew
+    git
+    base-cli
+    zsh
+    gpg
+    java
+    node
+    # work
+)
