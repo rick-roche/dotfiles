4e03abf964e97c26254d9956b60041e6dff1e8a0
Richard Roché <richard.j.roche@gmail.com>
2023-04-29 23:30:08 +0100

    fix: update dotnet module

--- modules/dotnet/_setup.zsh
+++ modules/dotnet/_setup.zsh
@@ -1,22 +1,14 @@
 #!/bin/zsh
 
 DIR=$(dirname "$0")
-ms_script_url='https://dotnet.microsoft.com/download/dotnet-core/scripts/v1/dotnet-install.sh'
-ms_script_name='dotnet-install.sh'
+. $DOTFILES_HOME/bin/_bootstrap.zsh
 
-curl -L --fail -o "$DIR"/"$ms_script_name" "$ms_script_url"
-chmod +x "$DIR"/"$ms_script_name"
-
-channels=('LTS' 'Current' '6.0')
-
-for ch in "${channels[@]}"; do
-    bash "$DIR"/"$ms_script_name" --channel "$ch"
-done
+module_brew_bundle $(basename $DIR)
 
 tools=('dotnet-outdated-tool' 'security-scan')
 
 for t in "${tools[@]}"; do
-    "$HOME"/.dotnet/dotnet tool update --global "$t"
+    dotnet tool update --global "$t"
 done
 
-rm -rf "$DIR"/"$ms_script_name"
+dotnet --list-sdks
