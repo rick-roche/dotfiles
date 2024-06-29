1b2d80d38a43ade1e2b548bfafb7b9016fad1608
Richard Roché <richard.j.roche@gmail.com>
2024-06-29 14:23:07 +0100

    chore(orko): add code dest, remove java, add raspberry-pi

--- settings/_orko-setup.zsh
+++ settings/_orko-setup.zsh
@@ -1,5 +1,6 @@
 export DOTFILES_PERSONALITY=orko
 export GPG_KEY=6DFF9D4160C0EE7FD656F0B7A54FEFD8636A433A
+export CODE_DEST="$HOME/code"
 
 . $DOTFILES_HOME/settings/_base-setup.zsh
 
@@ -9,7 +10,7 @@ DOTFILES_MODULES+=(
     docker
     dotnet
     golang
-    java
     node
     media
+    raspberry-pi
 )
