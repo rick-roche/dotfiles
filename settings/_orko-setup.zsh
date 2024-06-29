151292ce95a531cd01b2f53358b4e527a1abbf4d
Richard Roché <richard.j.roche@gmail.com>
2024-06-29 17:14:36 +0100

    chore: linter fixes

--- settings/_orko-setup.zsh
+++ settings/_orko-setup.zsh
@@ -6,7 +6,7 @@ export GPG_KEY=6DFF9D4160C0EE7FD656F0B7A54FEFD8636A433A
 export CODE_DEST="$HOME/code"
 
 # shellcheck disable=SC1091
-. $DOTFILES_HOME/settings/_base-setup.zsh
+. "$DOTFILES_HOME/settings/_base-setup.zsh"
 
 DOTFILES_MODULES+=(
     azure
